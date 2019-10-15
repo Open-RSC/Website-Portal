96aa6104ad87a3f3e6bf4ea4aa26144292275b92
Marwolf <marwolf@openrsc.com>
2019-10-15 00:35:33 -0400

    Adds a second logo

--- openrsc_web/resources/views/home.blade.php
+++ openrsc_web/resources/views/home.blade.php
@@ -1,14 +1,35 @@
 @extends('template')
 
 @section('content')
-    <div class="row">
-        <div class="col container text-center">
-            <div class="d-block pt-4">
-                <img src="{{ asset('img/logo.png') }}" class="img-fluid" alt="logo">
+    <div class="table text-center">
+        <div class="tr">
+            <div class="td">
+                <div class="pt-4">
+                    <img src="{{ asset('img/logo.png') }}" class="img-fluid" style="height: 200px;" alt="Open RSC">
+                </div>
+                <div class="pb-3">
+                    <div class="text-white-50">
+                        <ul>
+                            <li>- 1x XP rate</li>
+                            <li>- Right click "bank" on bankers</li>
+                        </ul>
+                    </div>
+                </div>
             </div>
 
-            <div class="d-block pb-3">
-                <div class="text-white-50">Striving for a replica RSC game and more</div>
+            <div class="td">
+                <div class="pt-4">
+                    <img src="{{ asset('img/cabbage_logo.png') }}" class="img-fluid" style="height: 200px;"
+                         alt="RSC Cabbage">
+                </div>
+                <div class="pb-3">
+                    <div class="text-white-50">
+                        <ul>
+                            <li>- 5x XP rate</li>
+                            <li>- All QoL features</li>
+                        </ul>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
