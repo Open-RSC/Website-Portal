5bb4f12903b59504953da3e401be98ae4f6cc5f0
Marwolf <cleako@gmail.com>
2021-01-31 08:17:04 -0500

    Upgrades PHPBB to 3.3.3 and fixes upgrade path in NGINX for board subdomain

--- board/config.php
+++ board/config.php
@@ -12,5 +12,4 @@ $phpbb_adm_relative_path = 'adm/';
 $acm_type = 'phpbb\\cache\\driver\\file';
 
 @define('PHPBB_INSTALLED', true);
-@define('PHPBB_ENVIRONMENT', 'production');
 // @define('DEBUG_CONTAINER', true);
