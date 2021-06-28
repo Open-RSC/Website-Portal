b92f0769673ff7d8e960d0f22b0054704bb6a5eb
Marwolf <marwolf@rsc.vet>
2021-06-28 17:39:05 -0400

    Updates hiscores skill page

--- portal/resources/views/hiscoreskill.blade.php
+++ portal/resources/views/hiscoreskill.blade.php
@@ -1,116 +1,116 @@
 @extends('template')
-
 @section('content')
-    <div class="col container">
-        <h2 class="h2 text-center pt-5 pb-4 text-capitalize display-3">Hiscores</h2>
-        <div class="row no-gutters">
+    <div style="width:450px;">
+        <div class="row">
 
-            <!-- placeholder left column -->
-            <div class="col"></div>
-
-            <!-- center column -->
-            <div class="col-auto" style="width: 600px;">
-                @if($subpage == 'hits')
-                    <div class="float-left h3 text-white">Fighting</div>
-                @else
-                    <div class="float-left h3 text-white">{{ ucfirst($subpage) }}</div>
-                @endif
-
-                <div class="float-right">
-                    <nav>
-                        <ul class="menu skill-dropdown">
-                            <li>
-                                <label for="drop-skills" class="toggle">Skills ▾</label>
-                                <a href="#" style="padding-top: 0; padding-bottom: 0;">
-                                    Skills
-                                </a>
-                                <input type="checkbox" id="drop-skills"/>
-                                <ul style="margin-top: -15px; margin-left: -6px;">
-                                    <li>
-                                        @foreach ($skill_array as $skill)
-                                            <a class="dropdown-item text-secondary"
-                                               href="/hiscores/{{ $skill }}">
-                                                <img src="{{ asset('img/skill_icons').'/'.$skill }}.svg"
-                                                     alt="{{ $skill }}" height="20px"/>
-                                                @if($skill == 'hits')
-                                                    Fighting
-                                                @elseif($skill == 'skill_total')
-                                                    Overall
-                                                @else
-                                                    {{ ucwords(preg_replace("/[^A-Za-z0-9 ]/", " ", $skill)) }}
-                                                @endif
-                                            </a>
-                                        @endforeach
-                                    </li>
-                                </ul>
-                            </li>
-                        </ul>
-                    </nav>
+            <div style="width:170px;">
+                <div style="padding-left: 10px;">
+                    <b>Select hiscore table</b>
                 </div>
-
-                <table id="List"
-                       class="container-fluid table-striped table-hover text-primary table-transparent">
-                    <thead class="border-bottom border-info thead-dark">
-                    <tr class="row text-info">
-                        <th class="col-1 text-right">Rank</th>
-                        <th class="col-sm-4 text-left">Player</th>
-                        <th class="col-1 text-right">Level</th>
-                        <th class="col-sm-3 text-left">XP</th>
-                        <th class="col-sm-3 text-right">Last Login</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    @foreach ($hiscores as $key=>$player)
-                        <tr class="row clickable-row" data-href="{{ route('player', $player->id) }}">
-                            <!--Rank-->
-                            <td class="col-1 text-right">
-                                <span>
-                                    {{ ($hiscores->currentpage()-1) * $hiscores->perpage() + $key + 1 }}
-                                </span>
-                            </td>
-                            <!--Player-->
-                            <td class="col-sm-4 text-left">
-                                <span>
-                                    {{ ucfirst($player->username) }}
-                                </span>
-                            </td>
-                            <!--Level-->
-                            <td class="col-1 text-right">
-                                <span>
-                                    {{ number_format((new App\Http\HiscoresController)->experienceToLevel($player->$subpage/4.0)) }}
-                                </span>
-                            </td>
-                            <!--XP-->
-                            <td class="col-sm-3 text-left">
-									<span>
-										{{ number_format($player->$subpage/4.0) }}
-									</span>
-                            </td>
-                            <!--Last Login-->
-                            <td class="col-sm-3 text-right">
-                                @if($player->login_date != 0)
-                                    <span>
-                                        {{ Carbon\Carbon::parse($player->login_date)->diffForHumans() }}
-                                    </span>
+                <div class="e bg-black p-2" style="outline: black;">
+                    @foreach ($skill_array as $skill)
+                        <div class="d-flex" style="padding-left:20px; padding-bottom:2px;">
+                            <div style="width:24px;">
+                                @if($skill == 'skill_total')
                                 @else
-                                    <span>Never</span>
+                                    <img src="{{ asset('img/skill_icons').'/'.$skill }}.gif" alt="{{ $skill }}"/>
                                 @endif
-                            </td>
-                        </tr>
+                            </div>
+                            <div style="width:40px;">
+                                <a class="col-3" href="/hiscores/{{ $skill }}">
+                                    @if($skill == 'skill_total')
+                                        Overall
+                                    @elseif($skill =='hits')
+                                        Hitpoints
+                                    @elseif($skill == 'woodcut')
+                                        Woodcutting
+                                    @elseif($skill == 'herblaw')
+                                        Herblore
+                                    @elseif($skill == 'runecraft')
+                                        Runecrafting
+                                    @else
+                                        {{ ucwords(preg_replace("/[^A-Za-z0-9 ]/", " ", $skill)) }}
+                                    @endif
+                                </a>
+                            </div>
+                        </div>
                     @endforeach
-                    </tbody>
-                </table>
-                {{ $hiscores->links('pagination::bootstrap-4') }}
+                </div>
             </div>
 
-            <!-- right column -->
-            <div class="col">
-                <div class="text-center">
-                    <label for="inputBox"></label>
-                    <input type="text" class="text-center" id="inputBox" onkeyup="search()"
-                           placeholder="Search this page" style="height: 33px;">
+            <div style="width:300px;">
+                <div style="padding-left: 75px;">
+                    @if($subpage ?? '' == null)
+                        <b>Overall Hiscores</b>
+                    @elseif($subpage ?? '' == 'hits')
+                        <b>Hitpoints Hiscores</b>
+                    @elseif($subpage ?? '' == 'woodcut')
+                        <b>Woodcutting Hiscores</b>
+                    @elseif($subpage ?? '' == 'herblaw')
+                        <b>Herblore Hiscores</b>
+                    @elseif($subpage ?? '' == 'runecraft')
+                        <b>Runecrafting Hiscores</b>
+                    @else
+                        <b>{{ ucfirst($subpage ?? '') }} Hiscores</b>
+                    @endif
+                </div>
+                <div class="e bg-black p-2" style="outline: black;">
+                    <div class="d-flex">
+                        <div class="text-right" style="width:40px;"><b>Rank</b></div>
+                        <div class="text-left" style="padding-left:10px; width:130px;"><b>Name</b></div>
+                        <div class="text-right" style="width:30px;"><b>Level</b></div>
+                        <div class="text-right" style="width:100px;"><b>XP</b></div>
+                    </div>
+                    @foreach ($hiscores as $key=>$player)
+                        <div class="d-flex clickable-row" data-href="{{ route('player', $player->id) }}">
+                            <!--Rank-->
+                            <div class="text-right" style="width:40px;">
+                                {{ ($hiscores->currentpage()-1) * $hiscores->perpage() + $key + 1 }}
+                            </div>
+                            <!--Player-->
+                            <div class="text-left" style="padding-left:10px; width:130px;">
+                                <a href="/player/{{ $player->id }}">{{ ucfirst($player->username) }}</a>
+                            </div>
+                            <!--Total Level-->
+                            <div class="text-right" style="padding-right:15px; width:30px;">
+                                {{ number_format((new App\Http\HiscoresController)->experienceToLevel($player->$subpage/4.0)) }}
+                            </div>
+                            <!--Total XP-->
+                            <div class="text-left" style="padding-left:10px; width:100px;">
+                                {{ number_format($player->$subpage/4.0) }}
+                            </div>
+                        </div>
+                    @endforeach
                 </div>
             </div>
         </div>
+        <!--{/{ $hiscores->links('pagination::simple-tailwind') }}-->
+    </div>
+
+    <div class="p-2"></div>
+
+    <div class="d-flex justify-content-center pl-4">
+        <div class="col hiscore-search">
+            <div class="search-box search-rank">
+                <form method="POST" role="search"><input type="hidden" name="_csrf" value="{{ csrf_token() }}">
+                    <label for="rank">Search by rank</label>
+                    <input id="rank" name="rank" type="text" required="required" style="width:100px;"
+                           class="bg-white text-black mt-1">
+                    <input type="submit" value="Search" aria-label="Search by rank" class="text-black pl-1 pr-1">
+                </form>
+            </div>
+        </div>
+
+        <div class="col">
+            <div class="search-box search-name" style="background-image:url("{{ asset('img/stoneback.gif') }}");">
+            <form method="POST" role="search">
+                <input type="hidden" name="_csrf" value="{{ csrf_token() }}">
+                <label for="name">Search by name</label>
+                <input id="name" name="name" type="text" required="required" style="width:100px;"
+                       class="bg-white text-black mt-1">
+                <input type="submit" value="Search" aria-label="Search by username" class="text-black pl-1 pr-1">
+            </form>
+        </div>
+    </div>
     </div>
 @endsection
