877774a3be623b9f115ab9506789efcc9fec4241
Marwolf <marwolf@rsc.vet>
2021-07-23 09:05:27 -0400

    Happy hiscore pagination

--- portal/resources/views/vendor/pagination/tailwind.blade.php
+++ portal/resources/views/vendor/pagination/tailwind.blade.php
@@ -1,119 +1,51 @@
 @if ($paginator->hasPages())
     <div aria-label="Pagination Navigation" class="pagination justify-content-center">
-        <div class="sm:hidden">
-            @if ($paginator->onFirstPage())
-                <span class="inline-flex items-center px-1 py-2 leading-5 rounded-md focus:outline-none"
-                      style="color:slategray; font-size:16px;">
-                    {!! __('pagination.previous') !!}
-                </span>
-            @else
-                <a href="{{ $paginator->previousPageUrl() }}"
-                   class="inline-flex items-center px-1 py-2 f leading-5 rounded-md hover:underline focus:outline-none"
-                   style="font-size:16px;">
-                    {!! __('pagination.previous') !!}
-                </a>
-            @endif
-
-            @if ($paginator->hasMorePages())
-                <a href="{{ $paginator->nextPageUrl() }}"
-                   class="inline-flex items-center px-1 py-2 f leading-5 rounded-md hover:underline focus:outline-none"
-                   style="font-size:16px;">
-                    {!! __('pagination.next') !!}
-                </a>
-            @else
-                <span class="inline-flex items-center px-1 py-2 leading-5 rounded-md focus:outline-none"
-                      style="color:slategray; font-size:16px;">
-                    {!! __('pagination.next') !!}
-                </span>
-            @endif
-        </div>
-
         <div class="hidden sm:flex-1 sm:flex sm:items-center sm:justify-center">
-
             <div>
-                <span class="relative z-0 inline-flex shadow-sm rounded-md">
-                    {{-- Previous Page Link --}}
-                    @if ($paginator->onFirstPage())
-                        <span aria-disabled="true" aria-label="{{ __('pagination.previous') }}">
-                            <span class="inline-flex items-center px-1 py-2 leading-5 rounded-md focus:outline-none"
-                                  style="color:slategray; font-size:16px;" aria-hidden="true">
-                                <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
-                                    <path fill-rule="evenodd"
-                                          d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
-                                          clip-rule="evenodd"/>
-                                </svg>
-                            </span>
-                        </span>
-                    @else
-                        <a href="{{ $paginator->previousPageUrl() }}" rel="prev"
-                           class="inline-flex items-center px-1 py-2 f leading-5 rounded-md hover:underline focus:outline-none"
-                           style="font-size:16px;" aria-label="{{ __('pagination.previous') }}">
-                            <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
-                                <path fill-rule="evenodd"
-                                      d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
-                                      clip-rule="evenodd"/>
-                            </svg>
-                        </a>
-                    @endif
+                <?php
+                $start = $paginator->currentPage() - 2; // show 3 pagination links before current
+                $end = $paginator->currentPage() + 2; // show 3 pagination links after current
+                if ($start < 1) {
+                    $start = 1; // reset start to 1
+                    $end += 1;
+                }
+                if ($end >= $paginator->lastPage()) $end = $paginator->lastPage(); // reset end to last page
+                ?>
 
-                    {{-- Pagination Elements --}}
-                    @foreach ($elements as $element)
+                @if($start > 1)
+                    <li class="inline-flex items-center px-1 py-2 leading-5 rounded-md focus:outline-none"
+                        style="color:slategray; font-size:16px;">
+                        <a class="inline-flex items-center px-1 py-2 f leading-5 rounded-md hover:underline focus:outline-none"
+                           href="{{ $paginator->url(1) }}">{{1}}</a>
+                    </li>
+                    @if($paginator->currentPage() != 4)
                         {{-- "Three Dots" Separator --}}
-                        @if (is_string($element))
-                            <span aria-disabled="true">
-                                <span class="inline-flex items-center px-1 py-2 leading-5 rounded-md focus:outline-none"
-                                      style="color:slategray; font-size:16px;">{{ $element }}</span>
-                            </span>
-                        @endif
-
-                        {{-- Array Of Links --}}
-                        @if (is_array($element))
-                            <?php $index = 0; ?>
-                            @foreach ($element as $page => $url)
-                                @if($index<4)
-                                    @if ($page == $paginator->currentPage())
-                                        <span aria-current="page">
-                                        <span class="inline-flex items-center px-1 py-2 leading-5 rounded-md focus:outline-none"
-                                              style="color:slategray; font-size:16px;">{{ $page }}</span>
-                                    </span>
-                                    @else
-                                        <a href="{{ $url }}"
-                                           class="inline-flex items-center px-1 py-2 f leading-5 rounded-md hover:underline focus:outline-none"
-                                           style="font-size:16px;"
-                                           aria-label="{{ __('Go to page :page', ['page' => $page]) }}">
-                                        {{ $page }}
-                                    </a>
-                                    @endif
-                                @endif
-                                <?php $index++ ?>
-                            @endforeach
-                        @endif
-                    @endforeach
-
-                    {{-- Next Page Link --}}
-                    @if ($paginator->hasMorePages())
-                        <a href="{{ $paginator->nextPageUrl() }}" rel="next"
-                           class="inline-flex items-center px-1 py-2 f leading-5 rounded-md hover:underline focus:outline-none"
-                           style="font-size:16px;" aria-label="{{ __('pagination.next') }}">
-                            <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
-                                <path fill-rule="evenodd"
-                                      d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-                                      clip-rule="evenodd"/>
-                            </svg>
-                        </a>
-                    @else
-                        <span aria-disabled="true" aria-label="{{ __('pagination.next') }}">
-                            <span class="inline-flex items-center px-1 py-2 leading-5 rounded-md focus:outline-none"
-                                  style="color:slategray; font-size:16px;" aria-hidden="true">
-                                <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
-                                    <path fill-rule="evenodd"
-                                          d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-                                          clip-rule="evenodd"/>
-                                </svg>
-                            </span>
-                        </span>
+                        <li class="inline-flex items-center px-1 py-2 leading-5 rounded-md focus:outline-none"
+                            style="color:slategray; font-size:16px;" aria-disabled="true"><span
+                                    class="inline-flex items-center px-1 py-2 leading-5 rounded-md focus:outline-none">...</span>
+                        </li>
                     @endif
-                </span>
+                @endif
+                @for ($i = $start; $i <= $end; $i++)
+                    <li class="inline-flex items-center px-1 py-2 leading-5 rounded-md focus:outline-none page-item {{ ($paginator->currentPage() == $i) ? ' active' : '' }}"
+                        style="color:slategray; font-size:16px;">
+                        <a class="inline-flex items-center px-1 py-2 f leading-5 rounded-md hover:underline focus:outline-none"
+                           href="{{ $paginator->url($i) }}">{{$i}}</a>
+                    </li>
+                @endfor
+                @if($end < $paginator->lastPage())
+                    @if($paginator->currentPage() + 3 != $paginator->lastPage())
+                        {{-- "Three Dots" Separator --}}
+                        <li class="inline-flex items-center px-1 py-2 leading-5 rounded-md focus:outline-none disabled"
+                            aria-disabled="true"><span
+                                    class="inline-flex items-center px-1 py-2 leading-5 rounded-md focus:outline-none">...</span>
+                        </li>
+                    @endif
+                    <li class="inline-flex items-center px-1 py-2 leading-5 rounded-md focus:outline-none">
+                        <a class="inline-flex items-center px-1 py-2 f leading-5 rounded-md hover:underline focus:outline-none"
+                           href="{{ $paginator->url($paginator->lastPage()) }}">{{$paginator->lastPage()}}</a>
+                    </li>
+                @endif
             </div>
         </div>
     </div>
