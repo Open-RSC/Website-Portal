019ae20fbd22b5e65b3fc624547926564bf1cb52
aldrichdev <aldrichdev@gmail.com>
2022-05-05 22:19:13 -0600

    WP-21 | Separated homepage online worlds table into regular and botting worlds tables, changed server links to all link to new Play Now page, updated play now graphic, added aria-labels for invisible play now buttons

---
+++ portal/resources/views/partials/homepage-botting-worlds-list.blade.php
@@ -0,0 +1,31 @@
+<table class="homepage-content homepage-online-worlds-table homepage-botting-worlds-list">
+    <tbody>
+        <tr>
+            <td colspan="2">
+                <p class="homepage-world-location">
+                    <img align="left" src="{{ asset('img/usflag.gif') }}" width="30" height="15" border="0">
+                    <span class="m-1">Raleigh</span>
+                </p>
+                <p class="homepage-world-type">Botting worlds</p>
+            </td>
+        </tr>
+        @foreach($worlds as $world)
+            @if(!$world["dev"] && $world["type"] == "cyborgs")
+                <tr>
+                    <td valign="center">
+                        <a href="/playnow"class="homepage-world-link">
+                            {{$world["name"]}}
+                        </a>
+                    </td>
+                    <td align="right">
+                        {{$world["online"]}} {{$world["type"]}}
+                    </td>
+                </tr>
+            @endif
+        @endforeach
+        <tr>
+            <td valign="center">&nbsp;</td>
+            <td align="right">&nbsp;</td>
+        </tr>
+    </tbody>
+</table>
--- portal/resources/views/playnow.blade.php
+++ portal/resources/views/playnow.blade.php
@@ -1,8 +1,10 @@
 @extends('template')
 @section('content')
     <div class="play-now-container">
-        <a href="/playnow/worldlist" class="play-now-left-button"></a>
-        <a href="{{ $gameClientUrl }}" class="play-now-right-button"></a>
+        <a href="/playnow/worldlist" class="play-now-left-button" 
+            aria-label="Play Open RSC servers via the web client"></a>
+        <a href="{{ $gameClientUrl }}" class="play-now-right-button"
+            aria-label="Play Open RSC servers via the game client (launcher)"></a>
         <img src="/img/PlayNowGraphic.png" class="play-now-image" 
             alt="Knight and Magician present two options for playing Open RSC servers" />
     </div>
