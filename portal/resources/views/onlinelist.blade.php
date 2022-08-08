0a018d095519f560b64af621a54a13ad65f268f1
Marwolf <marwolf@rsc.vet>
2022-08-07 21:47:26 -0400

    Minor online list page clean up

--- portal/resources/views/onlinelist.blade.php
+++ portal/resources/views/onlinelist.blade.php
@@ -4,14 +4,14 @@
 
     <span class="fw-bold text-center pb-2">
         There are {{$onlineCount}} players logged in to
-        <span style="color:yellow">
+        <span style="color: yellow;">
             {{$world}}
         </span>
     </span>
-    <table class="e hiscores-player-table" bgcolor="#000000" style="width: 450px;">
+    <table class="e" style="width: 450px; background: black">
         <tr>
             <th></th>
-            <th class="fw-bold pt-3 pl-3 pr-3 text-left">Name</th>
+            <th class="fw-bold pt-3 pl-6 pr-3 text-left">Name</th>
             <th class="fw-bold pt-3 text-left">Logged In</th>
             <th class="fw-bold pt-3 pl-6 pr-6 text-left">Cumulative Play</th>
         </tr>
@@ -20,47 +20,36 @@
                 <td class="pl-3">
                     @if($db== 'preservation')
                         <div class="ml-3 pb-1">
-                            <div class="rounded-circle" style="
-                                height: 48px;
-                                width: 48px;
-                                overflow: hidden;
-                                ">
-
+                            <div class="rounded-circle" style="height: 64px; width: 64px; overflow: hidden;">
                                 <a class="c" href="/player/{{ $db }}/{{ $player->username }}">
                                     <!-- Due to legacy OpenRSC database not following regular naming scheme -->
                                     <img src="{{ asset('/img/avatars').'/'.'openrsc'.'+'.$player->id }}.png"
                                          alt="({{ ucfirst($player->username) }})" onerror="this.style.display='none'"
-                                         style="
-                                        margin-top: -32px;
-                                        padding-top: 35px;
-                                        background-color: #0A0A0A;
-                                        "/>
+                                         style="margin-top: -32px; padding-top: 35px; background-color: #0A0A0A;"/>
                                 </a>
                             </div>
                         </div>
                     @else
-                        <a class="c" href="/player/{{ $db }}/{{ $player->username }}">
-                            <img src="{{ asset('/img/avatars').'/'.$db.'+'.$player->id }}.png"
-                                 alt="({{ ucfirst($player->username) }})" onerror="this.style.display='none'"/>
-                        </a>
+                        <div class="ml-3 pb-1">
+                            <div class="rounded-circle" style="height: 64px; width: 64px; overflow: hidden;">
+                                <a class="c" href="/player/{{ $db }}/{{ $player->username }}">
+                                    <img src="{{ asset('/img/avatars').'/'.$db.'+'.$player->id }}.png"
+                                         alt="({{ ucfirst($player->username) }})" onerror="this.style.display='none'"
+                                         style="margin-top: -32px; padding-top: 35px; background-color: #0A0A0A;"/>
+                                </a>
+                            </div>
+                        </div>
                     @endif
                 </td>
-                <td class="pl-3 pr-3 text-left">
+                <td class="pl-6 pr-3 text-left">
                     <a class="c" href="/player/{{ $db }}/{{ $player->username }}">{{ ucfirst($player->username) }}</a>
                 </td>
                 <td class="text-left">
-                    @if ($player->login_date)
-                        {{ App\Http\OnlineController::formattedTimeSince($player->login_date) }}
-                    @else
-                        Never
-                    @endif
+                    {{ App\Http\OnlineController::formattedTimeSince($player->login_date) }}
                 </td>
                 <td class="pl-6 pr-6 text-left">
-                    @if ($player->creation_date)
-                        {{ App\Http\OnlineController::formattedCumTime($player->value/1000, $player->login_date) }}
-                    @else
-                        Never
-                    @endif
+                    {{ App\Http\OnlineController::formattedCumTime($player->value/1000, $player->login_date) }}
+
                 </td>
             </tr>
         @endforeach
