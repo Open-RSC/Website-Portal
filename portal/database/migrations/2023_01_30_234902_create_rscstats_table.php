b9b1c27bffe1e4110ffc0b6ca65c5b30ce05e381
ipkpjersi <ipkpjersi@gmail.com>
2023-01-30 22:00:54 -0500

    Added rscstats migration
    This is probably fully correct and not missing anything, maybe.

---
+++ portal/database/migrations/2023_01_30_234902_create_rscstats_table.php
@@ -0,0 +1,62 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRscstatsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rscstats', function (Blueprint $table) {
+            $table->id();
+            $table->string('server');
+            $table->string('key'); //The key will be servername_stats_date_hour like cabbage_stats_2023-01-31_11PM to prevent re-creating the hourly stats.
+            $table->integer('online');
+            $table->integer('createdToday');
+            $table->integer('logins48');
+            $table->integer('uniquePlayers');
+            $table->integer('totalPlayers');
+            $table->unsignedBigInteger('sumGold');
+            $table->integer('gold1m');
+            $table->integer('gold5m');
+            $table->integer('gold10m');
+            $table->integer('pumpkin');
+            $table->integer('cracker');
+            $table->integer('redphat');
+            $table->integer('yellowphat');
+            $table->integer('bluephat');
+            $table->integer('greenphat');
+            $table->integer('pinkphat');
+            $table->integer('whitephat');
+            $table->integer('easteregg');
+            $table->integer('redmask');
+            $table->integer('bluemask');
+            $table->integer('greenmask');
+            $table->integer('santahat');
+            $table->integer('scythe');
+            $table->integer('dsq');
+            $table->integer('dmed');
+            $table->integer('dammy');
+            $table->integer('dbattle');
+            $table->integer('dlong');
+            $table->integer('rune2h');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rscstats');
+    }
+}
