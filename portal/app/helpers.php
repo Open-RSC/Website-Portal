6cbc526f5b51aa5d801c5f15137344568aeb368d
ipkpjersi <ipkpjersi@gmail.com>
2023-02-14 20:49:48 -0500

    Update helpers.php

--- portal/app/helpers.php
+++ portal/app/helpers.php
@@ -17,4 +17,33 @@ if (!function_exists('passwd_compat_hasher')) {
         return hash('sha512', ($salt . md5($credential)));
 
     }
+
+
+    if (!function_exists('add_characters')) {
+        function add_characters($s, $i)
+        {
+            $s1 = "";
+            for ($j = 0; $j < $i; $j++) {
+                if ($j >= strlen($s)) {
+                    $s1 .= " ";
+                } else {
+                    $c = $s[$j];
+                    $s1 .= ctype_alnum($c) ? $c : "_";
+                }
+            }
+            return $s1;
+        }
+    }
+
+    if (!function_exists('password_needs_rehashing')) {
+        /**
+         * @param $passwordHashed string The password hash
+         * @return bool
+         */
+        function password_needs_rehashing(string $passwordHashed): bool
+        {
+            return !str_starts_with($passwordHashed, "$2y$10$");
+        }
+
+    }
 }
