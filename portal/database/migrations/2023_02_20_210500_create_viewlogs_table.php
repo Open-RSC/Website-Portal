f1812912a71864a8a68fa18c3f85bff7d3d9550e
ipkpjersi <ipkpjersi@gmail.com>
2023-02-22 19:06:07 -0500

    Added info column to viewlogs table
    We shouldn't really modify the migration but since this isn't deployed yet it's fine.

--- portal/database/migrations/2023_02_20_210500_create_viewlogs_table.php
+++ portal/database/migrations/2023_02_20_210500_create_viewlogs_table.php
@@ -17,6 +17,7 @@ class CreateViewlogsTable extends Migration
             $table->id();
             $table->string("username");
             $table->string("page");
+            $table->string("info");
             $table->string("ip");
             $table->timestamps();
         });
