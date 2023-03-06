66c792523ee2cf47a8ba99ac5f6bca83a9fcd8fd
ipkpjersi <ipkpjersi@gmail.com>
2023-03-05 19:45:43 -0500

    Added session clear command
    This works great in combination with the ability to toggle logins to off or admin-only.

---
+++ portal/app/Console/Commands/ClearSessionsCommand.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Http\HiscoresController;
+use App\Services\Stats\StatsService;
+use Illuminate\Console\Command;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Storage;
+
+class ClearSessionsCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'sessions:clear';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Clear login sessions';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     * @return bool
+     */
+    public function handle()
+    {
+        $files = File::allFiles(storage_path('framework/sessions/'));
+        foreach($files as $file){
+            if ($file->getFilename() !== ".gitignore") {
+                File::delete(storage_path('framework/sessions/'.$file->getFilename())); 
+            }
+        }
+        return true;
+    }
+}
