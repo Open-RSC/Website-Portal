129b91d19d3642d19120ab4282b899f4e59b7e10
ipkpjersi <ipkpjersi@gmail.com>
2023-03-07 10:08:33 -0500

    Fixed viewlogs ip

--- portal/app/helpers.php
+++ portal/app/helpers.php
@@ -47,3 +47,24 @@ if (!function_exists('add_characters')) {
         return $s1;
     }
 }
+
+if (!function_exists('get_client_ip_address')) {
+    function get_client_ip_address()
+    {
+        if (isset($_SERVER["HTTP_CF_CONNECTING_IP"])) {
+            $_SERVER['REMOTE_ADDR'] = $_SERVER["HTTP_CF_CONNECTING_IP"];
+            $_SERVER['HTTP_CLIENT_IP'] = $_SERVER["HTTP_CF_CONNECTING_IP"];
+        }
+        $client = @$_SERVER['HTTP_CLIENT_IP'];
+        $forward = @$_SERVER['HTTP_X_FORWARDED_FOR'];
+        $remote = $_SERVER['REMOTE_ADDR'];
+        if (filter_var($client, FILTER_VALIDATE_IP)) {
+            $clientIp = $client;
+        } elseif (filter_var($forward, FILTER_VALIDATE_IP)) {
+            $clientIp = $forward;
+        } else {
+            $clientIp = $remote;
+        }
+        return $clientIp;
+    }
+}
