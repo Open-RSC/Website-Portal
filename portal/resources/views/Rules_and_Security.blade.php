f656b0ac7f22babafb6933c0a351574504e49f10
Marwolf <marwolf@rsc.vet>
2023-03-15 11:29:54 -0400

    Rules page update to match Discord

--- portal/resources/views/Rules_and_Security.blade.php
+++ portal/resources/views/Rules_and_Security.blade.php
@@ -2,34 +2,41 @@
 
 @section('content')
     <div class="text-gray-400 pr-5 pl-5 pt-3 pb-3 bg-black">
-        <span class="text-danger font-weight-bold d-block">Do not Advertise</span>
-        <span class="d-block">- on other Discord servers without staff permission</span>
-        <span class="d-block">- on external RSC-based games</span>
-        <span class="d-block">- Mass DM/PM is considered advertising</span>
 
-        <span class="text-danger font-weight-bold d-block pt-4">No RWT</span>
-        <span class="d-block">- Real World Trading (RWT)</span>
-        <span class="d-block">- Anything outside of the Open RSC games is considered RWT</span>
-        <span class="d-block">- No scamming</span>
+        <span class="text-danger font-weight-bold d-block pt-4">1️⃣ Be respectful & civil – language is flexible to a certain extent</span>
+        <span class="d-block">1.1 Don’t be rude and disrespectful</span>
+        <span class="d-block">1.1.1 Especially respect all moderator & admin ranks</span>
+        <span class="text-danger font-weight-bold d-block pt-4">1.2 Do not use alternate accounts to evade mutes and bans in order to continue harassing or bullying other players, nor writing generally inappropriate comments</span>
+        <span class="d-block">1.2.1 Do not use your friends to appeal mutes or bans either. Friends who appeal on behalf of others may be muted/banned themselves</span>
+        <span class="text-danger font-weight-bold d-block pt-4">1.3 No NSFW & NSFL content/discussions/player names</span>
+        <span class="text-danger font-weight-bold d-block pt-4">1.4 Do not flame or attack others</span>
+        <span class="d-block">1.4.1 No hate speech nor controversial topics such as politics, religion, race, LGBT, etc.</span>
+        <span class="d-block">1.4.2 No discrimination</span>
+        <span class="text-danger font-weight-bold d-block pt-4">1.5 No impersonating staff members</span>
+        <span class="d-block">1.6 Do not abuse tagging/mentions</span>
+        <span class="d-block">1.7 Do not encourage rule breaking</span>
+        <span class="d-block">1.8 No scamming & no gambling</span>
+        <span class="d-block">1.9 No offensive account names</span>
 
-        <span class="text-danger font-weight-bold d-block pt-4">Be respectful and civil</span>
-        <span class="d-block">– Language is flexible to a certain extent</span>
-        <span class="d-block">- Do not be rude and disrespectful</span>
-        <span class="d-block">- Be polite to all staff members</span>
-        <span class="d-block">- No posting illegal, NSFW, nor NSFL content</span>
-        <span class="d-block">- No heated discussions such as politics, religion, race, orientation</span>
-        <span class="d-block">- No impersonating staff members</span>
-        <span class="d-block">- Do not abuse tagging/mentions on Discord</span>
-        <span class="d-block">- Do not encourage rule breaking</span>
-        <span class="d-block">- No hosting “games of chance” (gambling)</span>
-        <span class="d-block">- Stay on topic in our Discord channels</span>
+        <span class="text-danger font-weight-bold d-block pt-4">2️⃣ Do not use bots, autos & macros in our games, the only exception being in RSC Uranium and RSC Coleslaw</span>
 
-        <span class="text-danger font-weight-bold d-block pt-4">No botting</span>
-        <span class="d-block">- Do not place bots, autos, nor macros inside our games</span>
-        <span class="d-block">&nbsp (except on RSC Uranium and RSC Coleslaw where it is allowed)</span>
-        <span class="d-block">- Do not exploit bugs, instead, report them and help out</span>
+        <span class="text-danger font-weight-bold d-block pt-4">3️⃣ Do not exploit vulnerabilities or bugs. Responsibly disclose them so they can be fixed. Bug abuse will result in permanent banning from all worlds, Discord, and other platforms</span>
+        <span class="d-block">3.1 In 2001scape, it is not allowed to create alt accounts to exploit quest rewards. The use of mules is allowed</span>
 
-        <span class="text-danger font-weight-bold d-block pt-4">Server specific rules</span>
-        <span class="d-block">- In 2001scape, it is not allowed to create alt accounts to exploit quest rewards. The use of mules is allowed.</span>
+        <span class="text-danger font-weight-bold d-block pt-4">4️⃣ No Real World Trading (RWT). Anything outside the Open RuneScape Classic game(s) (even from Jagex) is considered RWT</span>
+        <span class="d-block">4.1 No trading between botting (Uranium & Coleslaw) and non-botting (Preservation, Cabbage & 2001scape) worlds</span>
+        <span class="d-block">4.2 Account buying and selling is not allowed.</span>
+        <span class="d-block">4.2.1 Account sharing is not supported nor allowed. This is due to staff being unable to separate identities if one person breaks the rules and it is unfair to those competing for hiscores.</span>
+
+        <span class="text-danger font-weight-bold d-block pt-4">5️⃣ No illegal activity – follow all local laws and regulations (Discord etc.) terms of services</span>
+        <span class="d-block">5.1 Don’t tell people where to buy mind-altering substances, AKA drugs. Don’t endorse drugs unless they are also found in RuneScape Classic (i.e. Whiskey is okay)</span>
+
+        <span class="text-danger font-weight-bold d-block pt-4">6️⃣ No Advertising</span>
+        <span class="d-block">6.1 Other Discord Servers (without permission) is forbidden</span>
+        <span class="d-block">6.2 Other RSC-based private game servers or websites is forbidden (closed source and for-profit private servers)</span>
+        <span class="d-block">6.3 Any form of unwanted DM/PM advertising is forbidden</span>
+
+        <span class="text-danger font-weight-bold d-block pt-4">*️⃣ Members who violate any part of rule 1 will be muted, and if muted 3 times, removed from the discord.</span>
+
     </div>
 @endsection
