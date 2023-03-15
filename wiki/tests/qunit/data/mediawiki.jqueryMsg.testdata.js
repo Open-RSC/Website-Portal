032ae3378cf82a621c4e3daa35664e059d5f6a53
Marwolf <marwolf@rsc.vet>
2023-03-15 18:28:50 -0400

    Removes the baremetal mediawiki to replace with a docker instance

--- wiki/tests/qunit/data/mediawiki.jqueryMsg.testdata.js
+++
@@ -1,8 +0,0 @@
-module.exports.phpParserData = require( './mediawiki.jqueryMsg.data.json' );
-module.exports.initLang = function ( langCode ) {
-	try {
-		require( './' + langCode + '.js' );
-	} catch ( e ) {
-		// No custom JavaScript needed for this language
-	}
-};
