5bc94ce712dbfe2cc256997f176941c8e6effba7
ipkpjersi <ipkpjersi@gmail.com>
2023-04-07 22:55:11 -0400

    Added table population for new players

--- portal/app/Actions/Fortify/CreateNewUser.php
+++ portal/app/Actions/Fortify/CreateNewUser.php
@@ -37,7 +37,80 @@ class CreateNewUser implements CreatesNewUsers
             'email' => $input['email'],
             'password' => Hash::make($password)
         ])->save();
-        //$player->setDbConnection($input['db'])
+        DB::connection($input['db'])->table('maxstats')->insert(['playerID' => $player->id]);
+        DB::connection($input['db'])->table('curstats')->insert(['playerID' => $player->id]);
+        DB::connection($input['db'])->table('experience')->insert(['playerID' => $player->id]);
+        DB::connection($input['db'])->table('capped_experience')->insert(['playerID' => $player->id]);
+        $minLevels = [];
+        $experiences = [];
+        //We currently only allow for the original XP curve. This shouldn't be a problem.
+        if ($input['db'] === '2001scape') {
+            $minLevels["attack"] = 1;
+            $minLevels["defense"] = 1;
+            $minLevels["strength"] = 1;
+            $minLevels["hits"] = 10;
+            $minLevels["ranged"] = 1;
+            $minLevels["thieving"] = 1;
+            $minLevels["influence"] = 1;
+            $minLevels["praygood"] = 1;
+            $minLevels["prayevil"] = 1;
+            $minLevels["goodmagic"] = 1;
+            $minLevels["evilmagic"] = 1;
+            $minLevels["cooking"] = 1;
+            $minLevels["tailoring"] = 1;
+            $minLevels["woodcutting"] = 1;
+            $minLevels["firemaking"] = 1;
+            $minLevels["crafting"] = 1;
+            $minLevels["smithing"] = 1;
+            $minLevels["mining"] = 1;
+            $minLevels["herblaw"] = 1;
+        } else {
+            $minLevels["attack"] = 1;
+            $minLevels["defense"] = 1;
+            $minLevels["strength"] = 1;
+            $minLevels["hits"] = 10;
+            $minLevels["ranged"] = 1;
+            $minLevels["prayer"] = 1;
+            $minLevels["magic"] = 1;
+            $minLevels["cooking"] = 1;
+            $minLevels["woodcutting"] = 1;
+            $minLevels["fletching"] = 1;
+            $minLevels["fishing"] = 1;
+            $minLevels["firemaking"] = 1;
+            $minLevels["crafting"] = 1;
+            $minLevels["smithing"] = 1;
+            $minLevels["mining"] = 1;
+            $minLevels["herblaw"] = 1;
+            $minLevels["agility"] = 1;
+            $minLevels["thieving"] = 1;
+            if ($input['db'] === 'cabbage' || $input['db'] === 'coleslaw') {
+                $minLevels["runecraft"] = 1;
+                $minLevels["harvesting"] = 1;
+            }
+        }
+        
+        foreach ($minLevels as $key => $value) {
+            $experiences[$key] = 0;
+            if ($key === "hits") {
+                $experiences[$key] = 4000;
+            }
+        }
+        
+        foreach ($minLevels as $key => $value) {
+            DB::connection($input['db'])->table('curstats')
+            ->where('playerID', $player->id)
+            ->update([$key => $value]);
+            DB::connection($input['db'])->table('maxstats')
+            ->where('playerID', $player->id)
+            ->update([$key => $value]);
+        }
+        
+        foreach ($experiences as $key => $value) {
+            DB::connection($input['db'])->table('experience')
+            ->where('playerID', $player->id)
+            ->update([$key => $value]);
+        }
+        
         return $playerCreated;
     }
 
