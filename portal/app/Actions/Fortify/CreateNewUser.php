247f4a3ac01b5fcb750f2e595de43e35582a4bea
ipkpjersi <ipkpjersi@gmail.com>
2023-04-07 22:08:29 -0400

    Cleaned up and added additional create user validation
    This ensures that creating new users is always validated, no matter how we call CreateNewUser.

--- portal/app/Actions/Fortify/CreateNewUser.php
+++ portal/app/Actions/Fortify/CreateNewUser.php
@@ -4,8 +4,10 @@ namespace App\Actions\Fortify;
 
 use App\Models\players;
 use App\Models\User;
+use App\Traits\CreateUserValidation;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Redirect;
@@ -17,7 +19,7 @@ use function App\Helpers\add_characters;
 
 class CreateNewUser implements CreatesNewUsers
 {
-    use PasswordValidationRules;
+    use PasswordValidationRules, CreateUserValidation;
 
     /**
      * Validate and create a newly registered user.
@@ -26,13 +28,17 @@ class CreateNewUser implements CreatesNewUsers
      */
     public function create(array $input): bool | RedirectResponse |  null
     {
+        $this->validateCreateUserInput($input);
         $password = add_characters($input['password'], 20);
         $player = new players();
         $trimmed_username = trim(preg_replace('/[-_.]/', ' ', $input['username']));
-        return $player->setDbConnection($input['db'])->create([
+        $playerCreated = $player->setDbConnection($input['db'])->create([
             'username' => $trimmed_username,
             'email' => $input['email'],
             'password' => Hash::make($password)
         ])->save();
+        //$player->setDbConnection($input['db'])
+        return $playerCreated;
     }
+
 }
---
+++ portal/app/Traits/CreateUserValidation.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Traits;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rule;
+use Illuminate\Validation\ValidationException;
+
+trait CreateUserValidation
+{
+    protected function validateCreateUserRequest(Request $request)
+    {
+        $input = $request->all();
+        $this->validateCreateUserInput($input);
+    }
+    
+    protected function validateCreateUserInput(array $input)
+    {
+        Validator::make($input, [
+            'username' => ['bail', 'regex:/^([-a-z0-9_ ])+$/i', 'required', 'min:2', 'max:12'],
+            'email' => [
+                'required',
+                'string',
+                'email',
+                'max:255',
+            ],
+            'db' => ['required', Rule::in(['preservation', 'cabbage', '2001scape', 'coleslaw', 'uranium', 'openpk'])],
+            'password' => 'required|min:4|max:20',
+        ])->validate();
+        
+        $trimmed_username = trim(preg_replace('/[-_.]/', ' ', $input['username']));
+        
+        if (DB::connection($input['db'])->table('players')->where('email', '=', $input['email'])->exists()) {
+            throw ValidationException::withMessages([
+                'email' => [trans('The email address is already in use.')],
+            ]);
+        }
+        
+        if (DB::connection($input['db'])->table('players')->where('username', '=', $trimmed_username)->exists()) {
+            throw ValidationException::withMessages([
+                'email' => [trans('The username is already in use.')],
+            ]);
+        }
+    }
+}
