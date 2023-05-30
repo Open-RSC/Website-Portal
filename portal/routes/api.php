ce63da00a42c9a27133b7e8fcab510fac3d4780a
Shift <shift@laravelshift.com>
2023-05-30 22:24:48 +0000

    Adopt class based routes

--- portal/routes/api.php
+++ portal/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\PlayerController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -18,4 +19,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('playerexport/export/', 'PlayerController@exportSubmitApi')->middleware(['throttle:15,20'])->name('PlayerExportSubmit');
+Route::post('playerexport/export/', [PlayerController::class, 'exportSubmitApi'])->middleware(['throttle:15,20'])->name('PlayerExportSubmit');
