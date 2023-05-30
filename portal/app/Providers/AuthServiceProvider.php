1fb905349b5fa59ef829b1037083047bde4d4ab8
Shift <shift@laravelshift.com>
2023-05-30 22:24:38 +0000

    Convert string references to `::class`
    PHP 5.5.9 adds the new static `class` property which provides the fully qualified class name. This is preferred over using strings for class names since the `class` property references are checked by PHP.

--- portal/app/Providers/AuthServiceProvider.php
+++ portal/app/Providers/AuthServiceProvider.php
@@ -16,7 +16,7 @@ class AuthServiceProvider extends ServiceProvider
      */
     protected $policies = [
         // 'App\Models\Model' => 'App\Policies\ModelPolicy',
-        'App\Models\User' => 'App\Models\players',
+        \App\Models\User::class => \App\Models\players::class,
     ];
 
     /**
