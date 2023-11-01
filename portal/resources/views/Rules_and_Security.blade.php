6a757cde2be57f563d1552d7ff93c8dd231674a2
ipkpjersi <ipkpjersi@gmail.com>
2023-11-01 18:24:10 -0400

    Updated rules

--- portal/resources/views/Rules_and_Security.blade.php
+++ portal/resources/views/Rules_and_Security.blade.php
@@ -10,7 +10,7 @@
         <span class="d-block">1.2.1 Do not use your friends to appeal mutes or bans either. Friends who appeal on behalf of others may be muted/banned themselves</span>
         <span class="text-danger font-weight-bold d-block pt-4">1.3 No NSFW & NSFL content/discussions/player names</span>
         <span class="text-danger font-weight-bold d-block pt-4">1.4 Do not flame or attack others</span>
-        <span class="d-block">1.4.1 No hate speech nor controversial topics such as politics, religion, race, LGBT, etc.</span>
+        <span class="d-block">1.4.1 No hate speech or controversial topics such as Politics, Religion, Race, anti-LGBTQIA+ etc.</span>
         <span class="d-block">1.4.2 No discrimination</span>
         <span class="text-danger font-weight-bold d-block pt-4">1.5 No impersonating staff members</span>
         <span class="d-block">1.6 Do not abuse tagging/mentions</span>
@@ -26,15 +26,22 @@
         <span class="text-danger font-weight-bold d-block pt-4">4️⃣ No Real World Trading (RWT). Anything outside the OpenRSC game(s) (even from Jagex) is considered RWT</span>
         <span class="d-block">4.1 No trading between botting (Uranium & Coleslaw) and non-botting (Preservation, Cabbage & 2001scape) worlds</span>
         <span class="d-block">4.2 Account buying and selling is not allowed.</span>
-        <span class="d-block">4.2.1 Account sharing is not supported nor allowed. This is due to staff being unable to separate identities if one person breaks the rules and it is unfair to those competing for hiscores.</span>
+        <span class="d-block">4.2.1 Account sharing is not supported or allowed. This is due to staff being unable to separate identities if one person breaks the rules and it is unfair to those competing for hiscores.</span>
 
-        <span class="text-danger font-weight-bold d-block pt-4">5️⃣ No illegal activity – follow all local laws and regulations (Discord etc.) terms of services</span>
+        <span class="text-danger font-weight-bold d-block pt-4">5️⃣ No illegal activity – Follow all Government Laws & all involved (Discord etc.) Terms of Services</span>
         <span class="d-block">5.1 Don’t tell people where to buy mind-altering substances, AKA drugs. Don’t endorse drugs unless they are also found in RSC (i.e. Whiskey is okay)</span>
 
         <span class="text-danger font-weight-bold d-block pt-4">6️⃣ No Advertising</span>
         <span class="d-block">6.1 Other Discord Servers (without permission) is forbidden</span>
         <span class="d-block">6.2 Other RSC-based private game servers or websites is forbidden (closed source and for-profit private servers)</span>
         <span class="d-block">6.3 Any form of unwanted DM/PM advertising is forbidden</span>
+        <span class="d-block">6.4 Do not advertise Open RSC to others as part of unsolicited DM/PMs, or in other servers where permission has not been granted. This is not helpful to us. It is a bad look for the project to advertise Open RSC in spaces which are dedicated to other RuneScape communities.</span>
+
+        <span class="text-danger font-weight-bold d-block pt-4">*️⃣ We reserve the right to remove misbehaving players at our discretion even if they haven't explicitly broken any of the above rules.</span>
+        <span class="d-block">*️⃣ These rules are enforced at our discretion. We typically reach out to people before banning when they have behavioral problems or trouble getting along with others, but in some cases, people who don't seem like they will change their behaviour may be banned without the courtesy of a warning.</span>
+        <span class="d-block">*️⃣ Our moderation team are unpaid volunteers who have a limited capacity to withstand abuse. Insulting staff is a sure way to be removed from almost any community.</span>
+        <span class="d-block">*️⃣ We run this as a free service, open source so anyone can run a copy themselves. We also offer account exports, so that even if you are banned, you can take your account somewhere else.</span>
+
 
     </div>
 @endsection
