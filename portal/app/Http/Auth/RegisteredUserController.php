864e62d90791ea15fc30dcd896ceb4044c9a3d26
ipkpjersi <ipkpjersi@gmail.com>
2023-11-02 19:37:03 -0400

    Added registration warning, very useful for debugging

--- portal/app/Http/Auth/RegisteredUserController.php
+++ portal/app/Http/Auth/RegisteredUserController.php
@@ -7,6 +7,7 @@ use Illuminate\Auth\Events\Registered;
 use Illuminate\Http\Request;
 use Laravel\Fortify\Contracts\CreatesNewUsers;
 use Laravel\Fortify\Contracts\RegisterResponse;
+use function App\Helpers\get_client_ip_address;
 use function App\Helpers\is_incorrect_production_url;
 
 class RegisteredUserController extends \Laravel\Fortify\Http\Controllers\RegisteredUserController
@@ -16,6 +17,7 @@ class RegisteredUserController extends \Laravel\Fortify\Http\Controllers\Registe
     public function store(Request $request, CreatesNewUsers $creator): RegisterResponse
     {
         if (!config('openrsc.web_registration_enabled') || is_incorrect_production_url()) {
+            \Log::warning("IP " . get_client_ip_address() . " tried to validate with incorrect registration URL in production OR web registration not enabled!");
             return app(RegisterResponse::class);
         }
         $this->validateCreateUserRequest($request);
