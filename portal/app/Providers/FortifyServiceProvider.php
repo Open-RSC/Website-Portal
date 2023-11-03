4a1bc25371892b842c960e1deff41d4b195564fc
ipkpjersi <ipkpjersi@gmail.com>
2023-11-03 19:55:36 -0400

    Removed comment

--- portal/app/Providers/FortifyServiceProvider.php
+++ portal/app/Providers/FortifyServiceProvider.php
@@ -78,7 +78,7 @@ class FortifyServiceProvider extends ServiceProvider
                 $validated = $request->validate([
                     'username' => ['bail', 'regex:/^([a-zA-Z0-9_ ])+$/i', 'required', 'min:2', 'max:12'],
                     'password' => ['regex:/^([ -~])+$/i', 'required', 'min:4', 'max:20'],
-                    'db' => ['required', 'in:preservation,cabbage,uranium,coleslaw,openpk,2001scape'], // Validate the 'db' input
+                    'db' => ['required', 'in:preservation,cabbage,uranium,coleslaw,openpk,2001scape'],
                 ]);
             } catch (ValidationException $e) {
                 //\Log::info($e->validator->errors());
