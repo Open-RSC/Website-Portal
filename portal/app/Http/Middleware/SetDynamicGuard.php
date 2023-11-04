c077f1bb383049ad22de5c28695792c403f56804
ipkpjersi <ipkpjersi@gmail.com>
2023-11-03 20:47:04 -0400

    Clarify comment

--- portal/app/Http/Middleware/SetDynamicGuard.php
+++ portal/app/Http/Middleware/SetDynamicGuard.php
@@ -27,7 +27,7 @@ class SetDynamicGuard
             $player = new players();
             $player = $player->setConnection($guard)->find($userId);
             $playerUsername = trim(preg_replace('/[-_.]/', ' ', $player?->username ?? ""));
-            //This is probably entirely redundant, we already validated the user ID vs database from the login itself, but just in case we check anyway.
+            //This is probably entirely redundant, we already validated the user ID vs database from the login itself, but just in case we check for the correct user again anyway.
             if ($player === null || strtolower($playerUsername) !== strtolower($expectedUsername)) {
                 \Log::error("This shouldn't happen! player is null: " . ($player === null) . ", playerUsername: $playerUsername vs expectedUsername: $expectedUsername");
                 Auth::logout();
