5220d5a03a748982d658e690f398ad27f3583131
ipkpjersi <ipkpjersi@gmail.com>
2023-11-07 20:34:47 -0500

    Updated home page to have less misleading info/links

--- portal/resources/views/Home.blade.php
+++ portal/resources/views/Home.blade.php
@@ -448,7 +448,7 @@ obtainable item of your choice</span> as a thanks!
                                                     from our staff
                                                 </span>
                                             <div class="d-block">
-                                                <a href="/board/ucp.php?i=pm&folder=inbox" class="c">
+                                                <a href="/login" class="c">
                                                     Click Here
                                                 </a>
                                             </div>
@@ -526,7 +526,7 @@ obtainable item of your choice</span> as a thanks!
                                                 </tr>
                                                 </tbody>
                                             </table>
-                                            Manage your Password and Recovery Details
+                                            Manage your Account
                                             <div class="d-block">
                                                 <a href="/login" class="c">
                                                     Click Here
