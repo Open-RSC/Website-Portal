b179667a4f1a2efee60e76cfbe24f7f9c6972f41
ipkpjersi <ipkpjersi@gmail.com>
2023-11-08 23:21:06 -0500

    Fixed CF IP check and added IP error logging

--- portal/app/Http/Middleware/CheckBannedIp.php
+++ portal/app/Http/Middleware/CheckBannedIp.php
@@ -5,14 +5,27 @@ use App\Models\BannedIp;
 use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\IpUtils;
+use function App\Helpers\get_client_ip_address;
 
 class CheckBannedIp
 {
     public function handle(Request $request, Closure $next)
     {
         $bannedIps = BannedIp::pluck('ip_address')->toArray();
+        $ip = "";
 
-        if (IpUtils::checkIp($request->ip(), $bannedIps)) {
+        try {
+            $ip = get_client_ip_address();
+        } catch (\Exception $e) {
+            \Log::error("Error trying to get IP address of a user in CheckBannedIp, request IP is " . $request->ip() . ", Exception is " . $e->getMessage());
+        }
+
+        if (empty($ip)) {
+            \Log::error("Empty IP for a user, request IP is " . $request->ip() . ", skipping CheckBannedIp");
+            return $next($request);
+        }
+
+        if (IpUtils::checkIp($ip, $bannedIps)) {
             if ($request->expectsJson()) {
                 return response()->json(['message' => 'Your IP address has been banned.'], 403);
             }
