3864ebcced126ce991c9c9abcfdc7dc4bc3e3752
ipkpjersi <ipkpjersi@gmail.com>
2024-01-20 14:47:12 -0500

    Let's rotate, 13 times

--- portal/app/Rules/NoBadWordsRule.php
+++ portal/app/Rules/NoBadWordsRule.php
@@ -8,8 +8,10 @@ use Illuminate\Support\Str;
 
 class NoBadWordsRule implements ValidationRule
 {
-    // Banned words
-    protected array $badWords = ['ass', 'fuck', 'shit', 'suck', 'penis', 'vagina', 'pussy', 'whore', 'slut', 'dick', 'cock', 'cunt', 'mod', 'm0d', 'moderator', 'admin', 'adm1n', 'administrator', 'afman', 'jagex', 'java', 'phuck', 'fuk', 'fux', 'fuq', 'faq', 'foc', 'fok', 'fook', 'fek', 'fack', 'foek', 'feck', 'fcuk', 'fukc', 'fck', 'fuick', 'fhuck', 'phuk', 'chit', 'schit', 'shjt', 'shat', 'shet', 'siht', 'shti', 'sht', 'crap', 'bitch', 'bjtch', '13itch', 'bich', 'biatch', 'biotch', 'bastard', 'spastic', 'retard', 'niga', 'nigr', 'niger', 'chink', 'wop', 'coon', 'hitler', 'nazi', 'queer', 'kweer', 'fag', 'fagot', 'lesbo', 'bellend', 'genital', 'dik', 'wang', 'shlong', 'cok', 'pric', 'willy', 'boner', 'erection', 'bals', 'bollock', 'testicle', 'scrotum', 'nuts', 'clit', 'slit', 'vadge', 'fanny', 'twat', 'pusy', 'pusi', 'puss', 'breast', 'tit', 'tits', 'boob', 'niple', 'arse', 'trany', 'tranie', 'trannie', 'tranny', 'poo', 'poop', 'anus', 'rectum', 'anal', 'butt', 'asshole', 'assh01e', 'urinate', 'piss', 'urine', 'turd', 'faeces', 'excrement', 'excrete', 'fart', 'cack', 'sperm', 'cum', 'spunk', 'smeg', 'semen', 'ejaculat', 'rape', 'rapist', 'stalk', 'wank', 'masturbate', 'masterbating', 'pimp', 'prostitut', 'pedophile', 'paedophile', 'slaper', 'slag', 'lick', 'blojob', 'felat', 'cuniling', 'naked', 'undress', 'nude', 'condom', 'dildo', 'vibrator', 'bondage', 'spank', 'horny', 'throb', 'tampon', 'bloodrag', 'panty', 'porn', 'pasword', 'pass', 'pword', 'sieag heil', 'sieg heil', 'fak', 'facking', 'screw', 'nigga', 'modrune', 'ahole', 'negro', 'ljck', 'djck', 'tjts', 'hornie', 'musterbate', 'nigg'];
+    // Banned words using 13 rotations
+    protected array $badWords = ['nff', 'shpx', 'fuvg', 'fhpx', 'cravf', 'intvan', 'chffl', 'juber', 'fyhg', 'qvpx', 'pbpx', 'phag', 'zbq', 'z0q', 'zbqrengbe', 'nqzva', 'nqz1a', 'nqzvavfgengbe', 'nszna', 'wntrk', 'wnin', 'cuhpx', 'shx', 'shk', 'shd', 'snd', 'sbp', 'sbx', 'sbbx', 'srx', 'snpx', 'sbrx', 'srpx', 'sphx', 'shxp', 'spx', 'shvpx', 'suhpx', 'cuhx', 'puvg', 'fpuvg', 'fuwg', 'fung', 'furg', 'fvug', 'fugv', 'fug', 'penc', 'ovgpu', 'owgpu', '13vgpu', 'ovpu', 'ovngpu', 'ovbgpu', 'onfgneq', 'fcnfgvp', 'ergneq', 'avtn', 'avte', 'avtre', 'puvax', 'jbc', 'pbba', 'uvgyre', 'anmv', 'dhrre', 'xjrre', 'snt', 'sntbg', 'yrfob', 'oryyraq', 'travgny', 'qvx', 'jnat', 'fuybat', 'pbx', 'cevp', 'jvyyl', 'obare', 'rerpgvba', 'onyf', 'obyybpx', 'grfgvpyr', 'fpebghz', 'ahgf', 'pyvg', 'fyvg', 'inqtr', 'snaal', 'gjng', 'chfl', 'chfv', 'chff', 'oernfg', 'gvg', 'gvgf', 'obbo', 'avcyr', 'nefr', 'genal', 'genavr', 'genaavr', 'genaal', 'cbb', 'cbbc', 'nahf', 'erpghz', 'nany', 'ohgg', 'nffubyr', 'nffu01r', 'hevangr', 'cvff', 'hevar', 'gheq', 'snrprf', 'rkperzrag', 'rkpergr', 'sneg', 'pnpx', 'fcrez', 'phz', 'fchax', 'fzrt', 'frzra', 'rwnphyng', 'encr', 'encvfg', 'fgnyx', 'jnax', 'znfgheongr', 'znfgreongvat', 'cvzc', 'cebfgvghg', 'crqbcuvyr', 'cnrqbcuvyr', 'fyncre', 'fynt', 'yvpx', 'oybwbo', 'sryng', 'phavyvat', 'anxrq', 'haqerff', 'ahqr', 'pbaqbz', 'qvyqb', 'ivoengbe', 'obaqntr', 'fcnax', 'ubeal', 'guebo', 'gnzcba', 'oybbqent', 'cnagl', 'cbea', 'cnfjbeq', 'cnff', 'cjbeq', 'fvrnt urvy', 'fvrt urvy', 'snx', 'snpxvat', 'fperj', 'avttn', 'zbqehar', 'nubyr', 'arteb', 'ywpx', 'qwpx', 'gwgf', 'ubeavr', 'zhfgreongr', 'avtt'];
+
+    // Exact match banned words using 13 rotations
     protected array $exactMatchBadWords = [];
     /**
      * Create a new rule instance.
@@ -33,12 +35,12 @@ class NoBadWordsRule implements ValidationRule
      public function validate(string $attribute, mixed $value, Closure $fail): void
      {
          foreach ($this->badWords as $badWord) {
-            if (Str::contains(strtolower($value), $badWord)) {
+            if (Str::contains(strtolower($value), str_rot13($badWord))) {
                 $fail("The :attribute contains banned words.");
             }
         }
          foreach ($this->exactMatchBadWords as $badWord) {
-            if (strtolower($value) === $badWord) {
+            if (strtolower($value) === str_rot13($badWord)) {
                 $fail("The :attribute has a banned word.");
             }
         }
