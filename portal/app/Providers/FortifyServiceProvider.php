609bef3e6f79c3c72aef868304c2e816465e8016
ipkpjersi <ipkpjersi@gmail.com>
2024-01-28 21:47:17 -0500

    Added more logging for logging in

--- portal/app/Providers/FortifyServiceProvider.php
+++ portal/app/Providers/FortifyServiceProvider.php
@@ -16,6 +16,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Validation\ValidationException;
@@ -118,9 +119,31 @@ class FortifyServiceProvider extends ServiceProvider
                     try {
                         $ip = get_client_ip_address();
                     } catch (\Exception $e) {
-                        \Log::error("Error fetching ip address in FortifyServiceProvider authenticateUsing() for player $username database $database");
+                        \Log::error("Error fetching ip address in FortifyServiceProvider authenticateUsing() for player $username database $database, request IP is " . $request->ip() . ", Exception is " . $e->getMessage());
+                        if (Schema::hasTable('error_logs')) {
+                            DB::table('error_logs')->insert([
+                                'message' => "Error fetching ip address in FortifyServiceProvider authenticateUsing() for player $username database $database, request IP is " . $request->ip() . ", Exception is " . $e->getMessage(),
+                                'level' => 'error',
+                                'url' => $request->fullUrl() ?? "",
+                                'username' => $trimmed_username,
+                                'ip' => $ip,
+                                'created_at' => now(),
+                                'updated_at' => now(),
+                            ]);
+                        }
                     }
                     \Log::error("Player $trimmed_username IP $ip tried to log in to database $database but dynamic guard (or dynamic guard checker) did not run on the request, logging in to any database other than the default (preservation) is unsafe because player IDs can differ between databases, rejecting login!");
+                    if (Schema::hasTable('error_logs')) {
+                        DB::table('error_logs')->insert([
+                            'message' => "Player $trimmed_username IP $ip tried to log in to database $database but dynamic guard (or dynamic guard checker) did not run on the request, logging in to any database other than the default (preservation) is unsafe because player IDs can differ between databases, rejecting login!",
+                            'level' => 'error',
+                            'url' => $request->fullUrl() ?? "",
+                            'username' => $trimmed_username,
+                            'ip' => $ip,
+                            'created_at' => now(),
+                            'updated_at' => now(),
+                        ]);
+                    }
                     return false;
                 }
                 session(['db_connection' => $database, 'expected_username' => $trimmed_username]);
