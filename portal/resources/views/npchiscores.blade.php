c24e687be2300e651cb744766f781af5bbe4fa45
ipkpjersi <ipkpjersi@gmail.com>
2024-05-29 19:34:48 -0400

    Updated NPC search routing more

--- portal/resources/views/npchiscores.blade.php
+++ portal/resources/views/npchiscores.blade.php
@@ -102,7 +102,7 @@
             var dbName = document.getElementById('db').value;
             var npcName = document.getElementById('npcName').value.trim();
             if (npcName) {
-                window.location.href = `/searchNpcNameHiscores/${dbName}/${encodeURIComponent(npcName)}`;
+                window.location.href = `/searchNpcHiscoresByNpcName/${dbName}/${encodeURIComponent(npcName)}`;
             }
         }
         </script>
