878957e20b5a6fb0a64a0d9b94f3d61985c54493
Shift <shift@laravelshift.com>
2024-07-20 20:11:31 +0000

    Re-register HTTP middleware

--- portal/bootstrap/app.php
+++ portal/bootstrap/app.php
@@ -14,7 +14,99 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->redirectGuestsTo(fn () => route('login'));
+        $middleware->redirectUsersTo(RouteServiceProvider::HOME);
+
+        $middleware->web([
+            \App\Http\Middleware\CheckBannedIp::class,
+            \App\Http\Middleware\SetDynamicGuard::class,
+            \App\Http\Middleware\SetDynamicGuardChecker::class,
+        ]);
+
+        $middleware->throttleApi();
+        $middleware->api(\App\Http\Middleware\CheckBannedIp::class);
+
+        $middleware->group('cabbage', [
+            \App\Http\Middleware\EncryptCookies::class,
+            \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
+            \Illuminate\Session\Middleware\StartSession::class,
+            \Illuminate\Session\Middleware\AuthenticateSession::class,
+            \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+            \App\Http\Middleware\VerifyCsrfToken::class,
+            \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \App\Http\Middleware\CheckBannedIp::class,
+            \App\Http\Middleware\SetDynamicGuard::class,
+            \App\Http\Middleware\SetDynamicGuardChecker::class,
+        ]);
+
+        $middleware->group('preservation', [
+            \App\Http\Middleware\EncryptCookies::class,
+            \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
+            \Illuminate\Session\Middleware\StartSession::class,
+            \Illuminate\Session\Middleware\AuthenticateSession::class,
+            \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+            \App\Http\Middleware\VerifyCsrfToken::class,
+            \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \App\Http\Middleware\CheckBannedIp::class,
+            \App\Http\Middleware\SetDynamicGuard::class,
+            \App\Http\Middleware\SetDynamicGuardChecker::class,
+        ]);
+
+        $middleware->group('uranium', [
+            \App\Http\Middleware\EncryptCookies::class,
+            \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
+            \Illuminate\Session\Middleware\StartSession::class,
+            \Illuminate\Session\Middleware\AuthenticateSession::class,
+            \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+            \App\Http\Middleware\VerifyCsrfToken::class,
+            \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \App\Http\Middleware\CheckBannedIp::class,
+            \App\Http\Middleware\SetDynamicGuard::class,
+            \App\Http\Middleware\SetDynamicGuardChecker::class,
+        ]);
+
+        $middleware->group('coleslaw', [
+            \App\Http\Middleware\EncryptCookies::class,
+            \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
+            \Illuminate\Session\Middleware\StartSession::class,
+            \Illuminate\Session\Middleware\AuthenticateSession::class,
+            \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+            \App\Http\Middleware\VerifyCsrfToken::class,
+            \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \App\Http\Middleware\CheckBannedIp::class,
+            \App\Http\Middleware\SetDynamicGuard::class,
+            \App\Http\Middleware\SetDynamicGuardChecker::class,
+        ]);
+
+        $middleware->group('openpk', [
+            \App\Http\Middleware\EncryptCookies::class,
+            \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
+            \Illuminate\Session\Middleware\StartSession::class,
+            \Illuminate\Session\Middleware\AuthenticateSession::class,
+            \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+            \App\Http\Middleware\VerifyCsrfToken::class,
+            \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \App\Http\Middleware\CheckBannedIp::class,
+            \App\Http\Middleware\SetDynamicGuard::class,
+            \App\Http\Middleware\SetDynamicGuardChecker::class,
+        ]);
+
+        $middleware->group('2001scape', [
+            \App\Http\Middleware\EncryptCookies::class,
+            \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
+            \Illuminate\Session\Middleware\StartSession::class,
+            \Illuminate\Session\Middleware\AuthenticateSession::class,
+            \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+            \App\Http\Middleware\VerifyCsrfToken::class,
+            \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \App\Http\Middleware\CheckBannedIp::class,
+            \App\Http\Middleware\SetDynamicGuard::class,
+            \App\Http\Middleware\SetDynamicGuardChecker::class,
+        ]);
+
+        $middleware->alias([
+            'custom_throttle' => \App\Http\Middleware\CustomThrottling::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
