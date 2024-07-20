676c7da75beacda0952646a419b0d8a824777c8d
Shift <shift@laravelshift.com>
2024-07-20 20:11:32 +0000

    Re-register service providers

--- portal/bootstrap/app.php
+++ portal/bootstrap/app.php
@@ -6,7 +6,10 @@ use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
-    ->withProviders()
+    ->withProviders([
+        \Yajra\DataTables\DataTablesServiceProvider::class,
+        \ZanySoft\Zip\ZipServiceProvider::class,
+    ])
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         // api: __DIR__.'/../routes/api.php',
--- portal/config/app.php
+++ portal/config/app.php
@@ -7,23 +7,6 @@ return [
 
     'authentic' => env('AUTHENTIC', 'true'),
 
-    'providers' => ServiceProvider::defaultProviders()->merge([
-        /*
-         * Package Service Providers...
-         */
-
-        /*
-         * Application Service Providers...
-         */
-        App\Providers\AppServiceProvider::class,
-        App\Providers\AuthServiceProvider::class,
-        // App\Providers\BroadcastServiceProvider::class,
-        App\Providers\EventServiceProvider::class,
-        App\Providers\RouteServiceProvider::class,
-        App\Providers\FortifyServiceProvider::class,
-        Yajra\DataTables\DataTablesServiceProvider::class,
-        ZanySoft\Zip\ZipServiceProvider::class,
-    ])->toArray(),
 
     'aliases' => Facade::defaultAliases()->merge([
         'DataTables' => Yajra\DataTables\Facades\DataTables::class,
