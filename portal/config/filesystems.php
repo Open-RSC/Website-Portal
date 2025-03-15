61f8a68218a2e6c191b5732720ae0b99bc6a6557
ipkpjersi <ipkpjersi@gmail.com>
2025-03-14 23:15:48 -0400

    Fixed player exports for Laravel 12

--- portal/config/filesystems.php
+++ portal/config/filesystems.php
@@ -7,6 +7,10 @@ return [
             'driver' => 'local',
             'root' => base_path('sqlite'),
         ],
+        'local' => [
+            'driver' => 'local',
+            'root' => storage_path('app'), //Force Storage::disk('local') to use /storage/app instead of /storage/app/private.
+        ],
     ],
 
 ];
