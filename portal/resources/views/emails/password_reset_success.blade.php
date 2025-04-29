bfcc863d7cd5e6f44d341b0c44b2cdbbd5ae1b27
ipkpjersi <ipkpjersi@gmail.com>
2025-04-29 18:29:17 -0400

    Added logo to reset password emails

--- portal/resources/views/emails/password_reset_success.blade.php
+++ portal/resources/views/emails/password_reset_success.blade.php
@@ -1,6 +1,8 @@
 <table width="100%" cellpadding="0" cellspacing="0" style="background-color:#1c1c1c; color:#e0e0e0; font-family:Arial, sans-serif; padding:20px;">
     <tr>
         <td>
+            <img src="https://rsc.vet/img/logo.png" alt="OpenRSC Logo"
+            style="display:block; margin-bottom:20px; width:200px; height:auto;">
             <h2 style="color:#6be585;">OpenRSC ({{ \App\Helpers\uc_worlds($db) }}) Password Reset Confirmation</h2>
 
             <p style="margin-bottom:16px;">
