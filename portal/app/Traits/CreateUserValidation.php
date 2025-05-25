8e7d32fbd76e70dfc93c45abc57e151e877c56b9
ipkpjersi <ipkpjersi@gmail.com>
2025-05-25 16:55:47 -0400

    Require agree to rules and log creations

--- portal/app/Traits/CreateUserValidation.php
+++ portal/app/Traits/CreateUserValidation.php
@@ -22,14 +22,16 @@ trait CreateUserValidation
 
     protected function validateCreateUserInput(array $input)
     {
+        //We could add a custom captcha in here if we wanted.
         $rules = [
             'username' => ['bail', 'regex:/^([a-zA-Z0-9_ ])+$/i', 'required', 'min:2', 'max:12', new NoBadWordsRule],
             'email' => ['required', 'string', 'email', 'max:255'],
             'db' => ['required', Rule::in(['preservation', 'cabbage', '2001scape', 'coleslaw', 'uranium', 'openpk'])],
             'password' => ['regex:/^([ -~])+$/i', 'required', 'min:4', 'max:20', 'confirmed'],
+            'agree_to_rules' => ['required', 'accepted'],
         ];
         $inviteOnly = (Setting::where('key', 'invite_only_registration')->value('value') === '1') ?? false;
-        // Conditionally add invite code rules based on system configuration
+        //Conditionally add invite code rules based on system configuration
         if ($inviteOnly) {
             $rules['invite_code'] = ['required', 'string', 'exists:invite_codes,code,used,false'];
         }
@@ -52,7 +54,7 @@ trait CreateUserValidation
             ]);
         }
 
-        // Check if the username has already been badnamed
+        //Check if the username has already been badnamed
         $formerBadNameExists = DB::connection($db)->table('former_names')
             ->where(DB::raw('LOWER(formerName)'), '=', strtolower($trimmed_username))
             ->where('changeType', '=', 1)
@@ -64,7 +66,7 @@ trait CreateUserValidation
             ]);
         }
 
-        // Check if the user already has too many accounts
+        //Check if the user already has too many accounts
         $recentAccounts = DB::connection($db)->table('players')
             ->where('creation_ip', '=', get_client_ip_address())
             ->where('creation_date', '>=', time() - 86400)
